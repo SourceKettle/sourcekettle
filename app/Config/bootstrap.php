8e5d3a8bafd44ae1738daac0c154b9a264328694
Chris Bulmer <chriswbulmer@gmail.com>
2012-06-02 17:52:41 +0100

    Added TwitterBootstrap helper to the application
    Bootstrap can now be called using php by $this->Bootstrap->xxxxx
    
    See https://github.com/loadsys/twitter-bootstrap-helper for an API of how to use it (but replace TwitterBootstrap with just Bootstrap - it's less to write each time)

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -129,3 +129,5 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
+
+CakePlugin::load('TwitterBootstrap');
--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -23,13 +23,19 @@
 App::uses('Controller', 'Controller');
 
 /**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @package       app.Controller
- * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
- */
+*
+* AppController for the DevTrack system
+* The application wide controller.
+*
+* Licensed under The MIT License
+* Redistributions of files must retain the above copyright notice.
+* 
+* @copyright     DevTrack Development Team 2012
+* @link          http://github.com/chrisbulmer/devtrack
+* @package       DevTrack.Controller
+* @since         DevTrack v 0.1
+* @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+*/
 class AppController extends Controller {
+    public $helpers = array('Bootstrap' => array('className' => 'TwitterBootstrap.TwitterBootstrap'));
 }
