c0239275cb7d5851cc09b74839f89c52371d9c5f
Phillip Whittlesea <pw.github@thega.me.uk>
2012-06-04 14:50:55 +0100

    Multiple changes to Projects.index
    Fixed whitespace issues in ProjectsController

--- app/Controller/ProjectsController.php
+++ app/Controller/ProjectsController.php
@@ -18,27 +18,32 @@ App::uses('AppController', 'Controller');
 
 class ProjectsController extends AppController {
 
-	public $helpers = array('Time');
-	
+    /**
+     * Project helpers
+     * @var type 
+     */
+    public $helpers = array('Time');
+
     /**
      * index method
      *
      * @return void
      */
-	public function index() {
-		$this->Project->recursive = 0;
-		$this->set('projects', $this->paginate());
-	}
+    public function index() {
+        $this->Project->Collaborator->recursive = 0;
+        $this->set('projects', $this->Project->Collaborator->find('all', array('conditions' => array('Collaborator.user_id' => $this->Auth->user('id')))));
+        $this->paginate();
+    }
 
     /**
      * admin_index method
      *
      * @return void
      */
-	public function admin_index() {
-		$this->Project->recursive = 0;
-		$this->set('projects', $this->paginate());
-	}
+    public function admin_index() {
+        $this->Project->recursive = 0;
+        $this->set('projects', $this->paginate());
+    }
 
     /**
      * view method
@@ -46,13 +51,13 @@ class ProjectsController extends AppController {
      * @param string $id
      * @return void
      */
-	public function view($id = null) {
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		$this->set('project', $this->Project->read(null, $id));
-	}
+    public function view($id = null) {
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        $this->set('project', $this->Project->read(null, $id));
+    }
 
     /**
      * admin_view method
@@ -60,51 +65,61 @@ class ProjectsController extends AppController {
      * @param string $id
      * @return void
      */
-	public function admin_view($id = null) {
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		$this->set('project', $this->Project->read(null, $id));
-	}
+    public function admin_view($id = null) {
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        $this->set('project', $this->Project->read(null, $id));
+    }
 
     /**
-     * add method
+     * add
+     * Create a new project for the current user
      *
      * @return void
      */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->Project->create();
-			if ($this->Project->save($this->request->data)) {
-				$this->Session->setFlash(__('The project has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
-			}
-		}
-		$repoTypes = $this->Project->RepoType->find('list');
-		$this->set(compact('repoTypes'));
-	}
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Project->create();
+            if ($this->Project->save($this->request->data)) {
+
+                // Project has been saved
+                // Now to add the creator as the first admin user on the project
+                $data = array('Collaborator');
+                $data['Collaborator']['user_id'] = $this->Auth->user('id');
+                $data['Collaborator']['project_id'] = $this->Project->id;
+                $data['Collaborator']['access_level'] = 2;
+                $this->Project->Collaborator->save($data);
+
+                $this->Session->setFlash(__('The project has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+            }
+        }
+        $repoTypes = $this->Project->RepoType->find('list');
+        $this->set(compact('repoTypes'));
+    }
 
     /**
      * admin_add method
      *
      * @return void
      */
-	public function admin_add() {
-		if ($this->request->is('post')) {
-			$this->Project->create();
-			if ($this->Project->save($this->request->data)) {
-				$this->Session->setFlash(__('The project has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
-			}
-		}
-		$repoTypes = $this->Project->RepoType->find('list');
-		$this->set(compact('repoTypes'));
-	}
+    public function admin_add() {
+        if ($this->request->is('post')) {
+            $this->Project->create();
+            if ($this->Project->save($this->request->data)) {
+                $this->Session->setFlash(__('The project has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+            }
+        }
+        $repoTypes = $this->Project->RepoType->find('list');
+        $this->set(compact('repoTypes'));
+    }
 
     /**
      * edit method
@@ -112,24 +127,24 @@ class ProjectsController extends AppController {
      * @param string $id
      * @return void
      */
-	public function edit($id = null) {
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Project->save($this->request->data)) {
-				$this->Session->setFlash(__('The project has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Project->read(null, $id);
-		}
-		$repoTypes = $this->Project->RepoType->find('list');
-		$this->set(compact('repoTypes'));
-	}
+    public function edit($id = null) {
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Project->save($this->request->data)) {
+                $this->Session->setFlash(__('The project has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->Project->read(null, $id);
+        }
+        $repoTypes = $this->Project->RepoType->find('list');
+        $this->set(compact('repoTypes'));
+    }
 
     /**
      * admin_edit method
@@ -137,24 +152,24 @@ class ProjectsController extends AppController {
      * @param string $id
      * @return void
      */
-	public function admin_edit($id = null) {
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Project->save($this->request->data)) {
-				$this->Session->setFlash(__('The project has been saved'));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The project could not be saved. Please, try again.'));
-			}
-		} else {
-			$this->request->data = $this->Project->read(null, $id);
-		}
-		$repoTypes = $this->Project->RepoType->find('list');
-		$this->set(compact('repoTypes'));
-	}
+    public function admin_edit($id = null) {
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Project->save($this->request->data)) {
+                $this->Session->setFlash(__('The project has been saved'));
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The project could not be saved. Please, try again.'));
+            }
+        } else {
+            $this->request->data = $this->Project->read(null, $id);
+        }
+        $repoTypes = $this->Project->RepoType->find('list');
+        $this->set(compact('repoTypes'));
+    }
 
     /**
      * delete method
@@ -162,21 +177,21 @@ class ProjectsController extends AppController {
      * @param string $id
      * @return void
      */
-	public function delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		if ($this->Project->delete()) {
-			$this->Session->setFlash(__('Project deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Project was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+    public function delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        if ($this->Project->delete()) {
+            $this->Session->setFlash(__('Project deleted'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Project was not deleted'));
+        $this->redirect(array('action' => 'index'));
+    }
 
     /**
      * admin_delete method
@@ -184,19 +199,19 @@ class ProjectsController extends AppController {
      * @param string $id
      * @return void
      */
-	public function admin_delete($id = null) {
-		if (!$this->request->is('post')) {
-			throw new MethodNotAllowedException();
-		}
-		$this->Project->id = $id;
-		if (!$this->Project->exists()) {
-			throw new NotFoundException(__('Invalid project'));
-		}
-		if ($this->Project->delete()) {
-			$this->Session->setFlash(__('Project deleted'));
-			$this->redirect(array('action' => 'index'));
-		}
-		$this->Session->setFlash(__('Project was not deleted'));
-		$this->redirect(array('action' => 'index'));
-	}
+    public function admin_delete($id = null) {
+        if (!$this->request->is('post')) {
+            throw new MethodNotAllowedException();
+        }
+        $this->Project->id = $id;
+        if (!$this->Project->exists()) {
+            throw new NotFoundException(__('Invalid project'));
+        }
+        if ($this->Project->delete()) {
+            $this->Session->setFlash(__('Project deleted'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Project was not deleted'));
+        $this->redirect(array('action' => 'index'));
+    }
 }
--- app/View/Projects/index.ctp
+++ app/View/Projects/index.ctp
@@ -14,34 +14,22 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-echo $this->Bootstrap->page_header("Projects <small>all the projects you care about</small>"); ?>
+echo $this->Bootstrap->page_header("Projects <small>all the projects you care about</small>" .
+	$this->Bootstrap->button_link("New Project", array('action' => 'add'), array("style" => "primary", "size" => "medium", "class" => "pull-right"))); ?>
 
 <div class="row">
-	<div class="span8">
-		<?php // Loop through all the projects that a user has access to
-		foreach ($projects as $project): ?>
-		<div class="well">
-			<h3><?=$this->Html->link($project['Project']['name'], array('action' => 'view', $project['Project']['id']))?></h3>
-			<p><i class="icon-<?=($project['Project']['public']) ? 'globe' : 'lock' ?>"></i>
-			<?=$project['Project']['description']?></p>
-			<p>Last Modified: <?=$this->Time->timeAgoInWords($project['Project']['modified'])?></p>
-			<p><strong><?=$project['RepoType']['name']?></strong></p>
+	<?php // Loop through all the projects that a user has access to
+	foreach ($projects as $project): ?>
+		<div class="span4">
+			<div class="well project-well">
+				<h3 class="project-title"><?=$this->Html->link($project['Project']['name'], array('action' => 'view', $project['Project']['id']), array('class' => 'project-link'))?>
+				<i class="project-icon icon-<?=($project['Project']['public']) ? 'globe' : 'lock' ?>" style="float:right;"></i></h3>
+				<p class="project-desc"><?=$project['Project']['description']?></p>
+				<p class="project-time">Last Modified: <?=$this->Time->timeAgoInWords($project['Project']['modified'])?></p>
+			</div>
 		</div>
-		<?php endforeach; ?>
-		<?= $this->element('pagination') ?>
-	</div>
-	
-	<div class="span4">	
-		<div class="actions">
-			<h3><?php echo __('Actions'); ?></h3>
-			<ul>
-				<li><?php echo $this->Html->link(__('New Project'), array('action' => 'add')); ?></li>
-				<li><?php echo $this->Html->link(__('List Repo Types'), array('controller' => 'repo_types', 'action' => 'index')); ?> </li>
-				<li><?php echo $this->Html->link(__('New Repo Type'), array('controller' => 'repo_types', 'action' => 'add')); ?> </li>
-				<li><?php echo $this->Html->link(__('List Collaborators'), array('controller' => 'collaborators', 'action' => 'index')); ?> </li>
-				<li><?php echo $this->Html->link(__('New Collaborator'), array('controller' => 'collaborators', 'action' => 'add')); ?> </li>
-			</ul>
-		</div>
-	</div>
+	<?php endforeach; ?>
 </div>
-
+<div class="row">
+	<? //$this->element('pagination') ?>
+</div>
