5b81f896871fec58650d680780ca32213fdef66a
Chris Bulmer <chriswbulmer@gmail.com>
2012-06-05 16:29:30 +0100

    Formatted code and added method to display user account details.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Users Controller for the DevTrack system
@@ -18,38 +19,38 @@ App::uses('AppController', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
 
 class UsersController extends AppController {
-    
+
     public $uses = array('User', 'Setting', 'EmailConfirmationKey', 'SshKey');
-    
+
     public function beforeFilter() {
         parent::beforeFilter();
         $this->Auth->allow('register', 'activate', 'lost_password');
     }
-    
+
     /**
      * Function to allow users to register with the application
      */
-    public function register(){
+    public function register() {
         $this->set('title_for_layout', 'Register');
         //Check if registration is allowed by the user
         $enabled = $this->Setting->find('first', array('conditions' => array('name' => 'register_enabled')));
-        if ($enabled['Setting']['value']){ //Check the setting
+        if ($enabled['Setting']['value']) { //Check the setting
             //Registration part
-            if($this->request->is('post')){
+            if ($this->request->is('post')) {
                 $user = $this->User->findByEmail($this->request->data['User']['email']);
-                if (!empty($user)){
+                if (!empty($user)) {
                     $this->Session->setFlash("A user already exists with the email address specified.", 'default', array(), 'error');
                 } else {
-                
+
                     //if data was posted therefore a submitted form
                     if ($this->data['User']['password'] == $this->data['User']['password_confirm']) {
-                        if ($this->data['User']['password'] != 'password'){
+                        if ($this->data['User']['password'] != 'password') {
                             $this->User->create();
-                            if ($this->User->save($this->request->data['User'])){
+                            if ($this->User->save($this->request->data['User'])) {
                                 $id = $this->User->getLastInsertID();
 
                                 //Check to see if an SSH key was added and save it
-                                if (!empty($this->data['User']['ssh_key'])){
+                                if (!empty($this->data['User']['ssh_key'])) {
                                     $this->SshKey->create();
                                     $data = array('SshKey');
                                     $data['SshKey']['user_id'] = $id;
@@ -96,46 +97,50 @@ class UsersController extends AppController {
             $this->render('registration_disabled');
         }
     }
-    
+
+    public function api_register() {
+        
+    }
+
     /**
      * Generates a random key of a given length
      * @param type $length The length of the key
      * @return string The random key 
      */
-    private function generate_key($length){
-        $key = ""; 
-        $i = 0; 
-        $possible = "0123456789abcdfghjkmnpqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";  
- 
-        while ($i < $length){ 
-            $char = substr($possible, mt_rand(0, strlen($possible)-1), 1); 
-             
-            if (!strstr($key, $char)) {  
-                $key .= $char; 
-                $i++; 
-            } 
-        } 
+    private function generate_key($length) {
+        $key = "";
+        $i = 0;
+        $possible = "0123456789abcdfghjkmnpqrstvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        while ($i < $length) {
+            $char = substr($possible, mt_rand(0, strlen($possible) - 1), 1);
+
+            if (!strstr($key, $char)) {
+                $key .= $char;
+                $i++;
+            }
+        }
         return $key;
     }
-    
+
     /**
      * Used to activate user accounts
      * @param type $key The key used to activate an account
      */
-    public function activate($key = null){
-        if ($key == null){
+    public function activate($key = null) {
+        if ($key == null) {
             $this->Session->setFlash("The key given was not a valid activation key.", 'default', array(), 'error');
             $this->redirect('/');
         } else {
             $record = $this->EmailConfirmationKey->find('first', array('conditions' => array('key' => $key), 'recursive' => 1));
-            if(!empty($record)){
+            if (!empty($record)) {
                 $record['User']['is_active'] = 1;
 
                 //create a new record to stop it rehashing the password
                 $newrecord['User'] = array();
                 $newrecord['User']['id'] = $record['User']['id'];
                 $newrecord['User']['is_active'] = '1';
-                if ($this->User->save($newrecord['User'])){
+                if ($this->User->save($newrecord['User'])) {
                     $this->EmailConfirmationKey->delete($record['EmailConfirmationKey']['id'], false); //delete the email confirmation key
                     $this->Session->setFlash(__("<h4 class='alert-heading'>Success</h4>Your account is now activated. You can now login."), 'default', array(), 'success');
                     $this->redirect('/login');
@@ -144,23 +149,45 @@ class UsersController extends AppController {
                 }
             } else {
                 $this->Session->setFlash(__("<h4 class='alert-heading'>Error</h4>The link given was not valid. Please contact your system administrator to manually activate your account."), 'default', array(), 'error');
-
             }
             $this->redirect('/');
         }
     }
-    
+
     /**
      * Function to allow for users to reset their passwords
      */
-    public function lost_password(){
+    public function lost_password() {
         
     }
-    
+
     /**
      * Allows users to view their profile
      */
-    public function index(){
-        
-    } 
+    public function index() {
+        $user = $this->Auth->user();
+        $this->set('user', $user);
+        $this->User->id = $user['id'];
+        $this->request->data = $this->User->read();
+        $this->request->data['User']['password'] = null;
+    }
+    
+    
+
+    /**
+     * Allows admins to see all users
+     */
+    public function admin_index() {
+        $this->User->recursive = 0;
+        $this->set('users', $this->paginate());
+    }
+
+    public function admin_view($id = null) {
+        $this->User->id = $id;
+        if (!$this->User->exists()) {
+            throw new NotFoundException(__('Invalid user'));
+        }
+        $this->set('user', $this->User->read(null, $id));
+    }
+
 }
