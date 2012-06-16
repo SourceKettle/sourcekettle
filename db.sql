24f1bfabb238786d1c7dcf7c791c9d58bd3818ae
Jack Edge <yellowbounder@gmail.com>
2012-06-16 14:43:19 +0100

    Added SQL to add api_key table

--- db.sql
+++ db.sql
@@ -192,3 +192,14 @@ CREATE TABLE `source` (
   `project_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1
+
+-- Table structure for `api_keys`
+CREATE TABLE IF NOT EXISTS `api_keys` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `key` varchar(20) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
