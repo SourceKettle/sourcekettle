38785d6c545b208fa0805bc492c891ab8febe702
Alex Lo <acsl1g09@ecs.soton.ac.uk>
2012-10-05 15:55:43 +0100

    Fixed #174 - tasks shouldn't be able to be made dependent on themselves now

--- app/Model/Task.php
+++ app/Model/Task.php
@@ -186,6 +186,24 @@ class Task extends AppModel {
         );
     }
 
+	/**
+	 * beforeSave function
+	 *
+	 * @access public
+	 * @param array $options (default: empty array)
+	 * @return bool True if the save was successful.
+	 */
+	public function beforeSave(array $options = array()) {
+		foreach($this->data['DependsOn']['DependsOn'] as $key => $dependsOn) {
+			if ($dependsOn == $this->id) {
+				unset ($this->data['DependsOn'][$key]);
+				break;
+			}
+		}
+
+		return true;
+	}
+
     /**
      * isAssignee function.
      *
