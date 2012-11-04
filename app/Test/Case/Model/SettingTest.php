49b0f61d309a4da26672bec2d440d7d9dc8cfce6
Phillip Whittlesea <pw.github@thega.me.uk>
2012-11-04 14:36:31 +0000

    Added test cases for Setting model
    Test coverage on Model/Setting is 100%

--- app/Test/Case/Model/SettingTest.php
+++ app/Test/Case/Model/SettingTest.php
@@ -1,37 +1,89 @@
 <?php
+/**
+*
+* Setting Unit Tests for the DevTrack system
+*
+* Licensed under The MIT License
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright     DevTrack Development Team 2012
+* @link          http://github.com/chrisbulmer/devtrack
+* @package       DevTrack.Test.Case.Model
+* @since         DevTrack v 1.0
+* @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+*/
 App::uses('Setting', 'Model');
 
-/**
- * Setting Test Case
- *
- */
 class SettingTestCase extends CakeTestCase {
-/**
- * Fixtures
- *
- * @var array
- */
-	public $fixtures = array('app.setting');
 
-/**
- * setUp method
- *
- * @return void
- */
-	public function setUp() {
-		parent::setUp();
-		$this->Setting = ClassRegistry::init('Setting');
-	}
+    /**
+     * setUp function.
+     * Run before each unit test.
+     * Corrrecly sets up the test environment.
+     *
+     * @access public
+     * @return void
+     */
+    public $fixtures = array('app.setting');
 
-/**
- * tearDown method
- *
- * @return void
- */
-	public function tearDown() {
-		unset($this->Setting);
+    /**
+     * setUp function.
+     * Run before each unit test.
+     * Corrrecly sets up the test environment.
+     *
+     * @access public
+     * @return void
+     */
+    public function setUp() {
+        parent::setUp();
+        $this->Setting = ClassRegistry::init('Setting');
+    }
 
-		parent::tearDown();
-	}
+    /**
+     * tearDown function.
+     * Tear down all created data after the tests.
+     *
+     * @access public
+     * @return void
+     */
+    public function tearDown() {
+        unset($this->Setting);
 
+        parent::tearDown();
+    }
+
+    /**
+     * test Setting->syncRequired function.
+     * Tests that the sync required function sets the sync flag.
+     *
+     * @access public
+     * @return void
+     */
+    public function testSyncRequired() {
+        $beforeA = $this->Setting->findByName('sync_required');
+        $beforeB = array(
+            'Setting' => array(
+                'id' => 1,
+                'name' => 'sync_required',
+                'value' => 0,
+                'created' => '2012-06-02 20:05:59',
+                'modified' => '2012-06-02 20:05:59'
+            ),
+        );
+        $this->assertEquals($beforeA, $beforeB, "before settings arnt equal");
+
+        $this->Setting->syncRequired();
+
+        $afterA = $this->Setting->findByName('sync_required');
+        $afterB = array(
+            'Setting' => array(
+                'id' => 1,
+                'name' => 'sync_required',
+                'value' => 1,
+                'created' => '2012-06-02 20:05:59',
+                'modified' => '2012-06-02 20:05:59'
+            ),
+        );
+        $this->assertEquals($beforeA, $beforeB, "after settings arnt equal");
+    }
 }
--- app/Test/Fixture/SettingFixture.php
+++ app/Test/Fixture/SettingFixture.php
@@ -1,37 +1,24 @@
 <?php
-/**
- * SettingFixture
- *
- */
+
 class SettingFixture extends CakeTestFixture {
 
-/**
- * Fields
- *
- * @var array
- */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 4, 'key' => 'primary'),
-		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'unique', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'value' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'name' => array('column' => 'name', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
-	);
+    public $fields = array(
+        'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 4, 'key' => 'primary'),
+        'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'unique', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'value' => array('type' => 'string', 'null' => false, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+        'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+        'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+        'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'name' => array('column' => 'name', 'unique' => 1)),
+        'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+    );
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-		array(
-			'id' => 1,
-			'name' => 'Lorem ipsum dolor sit amet',
-			'value' => 'Lorem ipsum dolor sit amet',
-			'created' => '2012-06-02 20:05:59',
-			'modified' => '2012-06-02 20:05:59'
-		),
-	);
+    public $records = array(
+        array(
+            'id' => 1,
+            'name' => 'sync_required',
+            'value' => 0,
+            'created' => '2012-06-02 20:05:59',
+            'modified' => '2012-06-02 20:05:59'
+        ),
+    );
 }
