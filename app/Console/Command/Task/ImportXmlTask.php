b5f99172a50614570399159e17ee777a7169e719
Phillip Whittlesea <pw.github@thega.me.uk>
2013-01-14 22:18:27 +0000

    Issue #256 - Add simple XML export functionality
    Note that this does not filter the data in any way

---
+++ app/Console/Command/Task/ImportXmlTask.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ *
+ * ImportXmlTask Task for the DevTrack system
+ * A task for importing data in an XML format
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     DevTrack Development Team 2012
+ * @link          http://github.com/chrisbulmer/devtrack
+ * @package       DevTrack.Console.Command.Task
+ * @since         DevTrack v 1.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+class ImportXmlTask extends Shell {
+    /**
+     * fileParamNumber
+     * The number that is used to fetch the
+     * output file from input params
+     *
+     * @var int
+     * @access private
+     */
+    private $fileParamNumber = 5;
+
+    /**
+     * rootXMLElement
+     * The root XML element
+     *
+     * @var string
+     * @access private
+     */
+    private $rootXMLElement = 'codekettle';
+
+    /**
+     * uses
+     * The models which will be exported by this task
+     *
+     * @var mixed
+     * @access public
+     */
+    public $uses = array(
+        'Settings',
+        'Users',
+        'Projects',
+        'Collaborators',
+        'Times',
+        'Milestones',
+        'Tasks',
+        'SshKeys',
+        'ProjectHistories',
+    );
+
+    /**
+     * _checkStartupParams function.
+     * Check the parameters that have been given to us
+     *
+     * @access private
+     * @param mixed $params
+     */
+    private function _checkStartupParams($params) {
+        if (isset($params['argv'][$this->fileParamNumber])) {
+            $file = $params['argv'][$this->fileParamNumber];
+            if (substr($file, 0, strlen('--'))=='--') {
+                $this->_criticalError('Please specify a valid output file');
+            }
+        } else {
+            $this->_criticalError('Please specify a valid output file');
+        }
+    }
+
+    /**
+     * _critcalError function.
+     * Prints a formatted error to the screen and exits the app
+     *
+     * @access private
+     * @param string $reason reason for exiting
+     */
+    private function _criticalError($reason) {
+        $this->out('<error>Error:</error> ' . $reason, 1, Shell::NORMAL);
+        exit(1);
+    }
+
+    /**
+     * _genericGather function.
+     * Gather the XML and insert into the database
+     *
+     * @param mixed $item The item to set the content as
+     * @param mixed $items The items to iterate over
+     * @param mixed $search The item to pull out of items
+     * @return mixed array of result
+     */
+    private function _genericGather($item, $items, $search) {
+        $insertions = $items[$item];
+        if (is_array($insertions)) {
+            if (isset($insertions['id'])) {
+                $this->out('About to insert ' . $search . ' ' . $insertions['id'], 1, Shell::VERBOSE);
+                if ($this->{$search}->save($insertions)) {
+                    $this->out('Inserted ' . $search . ' ' . $insertions['id'], 1, Shell::VERBOSE);
+                } else {
+                    $this->out('<warning>Failed to insert ' . $search . ' ' . $insertions['id'] . '</warning>', 1, Shell::VERBOSE);
+                }
+            } else {
+                foreach ($insertions as $insertion) {
+                    $this->out('About to insert ' . $search . ' ' . $insertion['id'], 1, Shell::VERBOSE);
+                    if ($this->{$search}->save($insertion)) {
+                        $this->out('Inserted ' . $search . ' ' . $insertion['id'], 1, Shell::VERBOSE);
+                    } else {
+                        $this->out('<warning>Failed to insert ' . $search . ' ' . $insertion['id'] . '</warning>', 1, Shell::VERBOSE);
+                    }
+                }
+            }
+        }
+    }
+    /**
+     * execute function.
+     * The main import function
+     *
+     * @access public
+     * @param array $params (default: array())
+     * @return void
+     */
+    public function execute($params = array()) {
+        $this->_checkStartupParams($params);
+
+        $inputFile = new File($params['argv'][$this->fileParamNumber]);
+        if (!$inputFile->exists()) {
+            $this->_criticalError('Input file does not exist');
+        }
+        $this->out('About to import from ' . $inputFile->path, 1, Shell::VERBOSE);
+
+        $inputArray = array();
+        try {
+            $inputArray = Xml::toArray(Xml::build($inputFile->read()));
+        } catch (Exception $e) {
+            $this->_criticalError($e->getMessage());
+        }
+
+        foreach ($this->uses as $a => $from) {
+            $toGroup = strtolower($from);
+            $toSingle = Inflector::singularize($toGroup);
+            if (isset($inputArray[$this->rootXMLElement][$toGroup])) {
+                $this->_genericGather($toSingle, $inputArray[$this->rootXMLElement][$toGroup], $from);
+            }
+        }
+    }
+}
--- app/Console/Command/XMLShell.php
+++ app/Console/Command/XMLShell.php
@@ -1,55 +1,54 @@
 <?php
-
 /**
- * Description of XMLShell
  *
- * @author pwhittlesea
+ * XmlShell Shell for the DevTrack system
+ * A shell for importing/exporting data in an XML format
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     DevTrack Development Team 2012
+ * @link          http://github.com/chrisbulmer/devtrack
+ * @package       DevTrack.Console.Command
+ * @since         DevTrack v 1.0
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
+class XmlShell extends AppShell {
 
-App::uses('AuthComponent', 'Controller/Component');
-class XMLShell extends AppShell {
+    public $tasks = array('ImportXml', 'ExportXml');
 
-    public $uses = array('Project', 'User');
+    /**
+     * import function.
+     * Import data
+     *
+     * @access public
+     * @return void
+     */
+    public function import() {
+        $this->ImportXml->execute($this->_collectParameters());
+    }
 
-    // Cache
-    var $_element = null;
-    var $_content = null;
-    var $_collaborators = array();
+    /**
+     * export function.
+     * Export data
+     *
+     * @access public
+     * @return void
+     */
+    public function export() {
+        $this->ExportXml->execute($this->_collectParameters());
+    }
 
-    var $domain;
-    var $date;
-
-    // Booleans
-    var $_in_project = false;
-    var $_in_user = false;
-
-    // XML format
-    var $_format = array(
-        'devtrackHeader' => array(
-            'created',
-            'domain',
-        ),
-        'devtrackBody' => array(
-            'project' => array(
-                'name',
-                'description',
-                'isPublic',
-                'created',
-                'tasks',
-                'source',
-//                'collaborator',
-            ),
-            'user' => array(
-                'email',
-                'name',
-                'password',
-                'is_active',
-                'is_admin',
-                'created',
-//                'sshKey',
-            ),
-        )
-    );
+    /**
+     * _collectParameters function.
+     * Collect all of the variables needed for execution
+     *
+     * @access private
+     * @return array of server params
+     */
+    private function _collectParameters() {
+        return array_merge($_SERVER, $_ENV);
+    }
 
     /**
      * main function.
@@ -58,195 +57,7 @@ class XMLShell extends AppShell {
      * @return void
      */
     public function main() {
-        $vars = array_merge($_SERVER, $_ENV);
-
-        $file = $vars['argv'][sizeof($vars['argv']) - 1];
-
-        if (is_null($file) || $file == '' || !file_exists($file)) {
-            print "Please specify a valid XML file to parse\n";
-            exit(1);
-        }
-
-        $this->date = date('Y-m-d H:i:s', time());
-        /* create the parser */
-        $parser = xml_parser_create();
-        xml_set_element_handler($parser, array($this,'startElemHandler'), array($this,'endElemHandler'));
-        xml_set_character_data_handler($parser, array($this,'character_data'));
-        xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
-
-        $string = file_get_contents($file);
-        $string = preg_replace('/[\x00-\x1F\x7F]/', '', $string);
-
-        // output each link
-        xml_parse($parser, $string);
-
-        // clean up - we're done
-        xml_parser_free($parser);
-    }
-
-    /**
-     * startElemHandler function.
-     *
-     * @access private
-     * @param mixed $parser
-     * @param mixed $name
-     * @param mixed $attribs
-     * @return void
-     */
-    private function startElemHandler($parser, $name, $attribs) {
-        if ( $name == 'project' ) {
-            $this->_element = array(
-                'Collaborators' => array()
-            );
-            $this->_in_project = true;
-        } else if ( $name == 'user' ) {
-            $this->_element = array(
-                'SshKeys' => array()
-            );
-            $this->_in_user = true;
-        }
-    }
-
-    /**
-     * endElemHandler function.
-     *
-     * @access private
-     * @param mixed $parser
-     * @param mixed $name
-     * @return void
-     */
-    private function endElemHandler($parser, $name) {
-        if ( $this->_in_project ) {
-            // Project handling code
-            if ( in_array($name, $this->_format['devtrackBody']['project']) ) {
-                $this->_element[$name] = $this->_content;
-            } else if ( $name == 'collaborator' ) {
-                $this->_element['Collaborators'][] = $this->_content;
-            } else if ( $name == 'project' ) {
-
-                // Handle the Project
-                $this->_in_project = false;
-                $this->handleProject();
-
-            }
-        } else if ( $this->_in_user ) {
-            // User handling code
-            if ( in_array($name, $this->_format['devtrackBody']['user']) ) {
-                $this->_element[$name] = $this->_content;
-            } else if ( $name == 'sshKey' ) {
-                $this->_element['SshKeys'][] = $this->_content;
-            } else if ( $name == 'user' ) {
-
-                // Handle the User
-                $this->_in_user = false;
-                $this->handleUser();
-
-            }
-        } else {
-            if ( $name == 'domain' ) {
-                $this->domain = $this->_content;
-            }
-        }
-    }
-
-    /**
-     * character_data function.
-     *
-     * @access private
-     * @param mixed $parser
-     * @param mixed $data
-     * @return void
-     */
-    private function character_data($parser, $data) {
-        $this->_content = $data;
-    }
-
-    /**
-     * handleProject function.
-     *
-     * @access private
-     * @return void
-     */
-    private function handleProject() {
-        $errs = false;
-
-        // Validate the Project
-        foreach ($this->_format['devtrackBody']['project'] as $val) {
-
-            if ( !isset($this->_element[$val]) ) {
-                echo "[ERROR] Project ".$this->_element['name']." is missing element ${val}\n";
-                $errs = true;
-            } else if ( is_null($this->_element[$val]) ) {
-                echo "[WARN] Project ".$this->_element['name']." has null ${val}\n";
-            }
-
-        }
-
-        // If the project Validates continue
-        if (!$errs) {
-            $this->_element['repo_type'] = (int) 3;
-            $this->_element['created'] = date('Y-m-d H:i:s', (int) $this->_element['created']);
-            $this->_element['modified'] = $this->date;
-
-            $this->_collaborators[$this->_element['name']] = $this->_element['Collaborators'];
-            unset($this->_element['Collaborators']);
-
-            // Store the Project in the DB
-            $this->Project->create();
-            if (!$this->Project->save(array('Project'=>$this->_element))) {
-                echo "[ERROR] Project \"".$this->_element['name']."\" was not saved\n";
-                echo "[ERROR] Validation failed:\n";
-                foreach ($this->Project->invalidFields() as $x => $err) {
-                    foreach (array_unique($err) as $e) {
-                        echo "[ERROR] Field \"${x}\" -> ${e}\n";
-                    }
-                }
-            } else {
-                echo "[INFO] Project \"".$this->_element['name']."\" saved successfully\n";
-            }
-        }
-    }
-
-    /**
-     * handleUser function.
-     *
-     * @access private
-     * @return void
-     */
-    private function handleUser() {
-        $errs = false;
-
-        // Validate the User
-        foreach ($this->_format['devtrackBody']['user'] as $val) {
-
-            if ( !isset($this->_element[$val]) ) {
-                echo "[ERROR] User ".$this->_element['email']." is missing element ${val}\n";
-                $errs = true;
-            } else if ( is_null($this->_element[$val]) ) {
-                echo "[WARN] User ".$this->_element['email']." has null ${val}\n";
-            }
-
-        }
-
-        // If the user Validates continue
-        if (!$errs) {
-            $this->_element['created'] = date('Y-m-d H:i:s', (int) $this->_element['created']);
-            $this->_element['modified'] = $this->date;
-
-            $this->User->create();
-            if (!$this->User->save(array('User'=>$this->_element))) {
-                echo "[ERROR] User ".$this->_element['email']." was not saved\n";
-                echo "[ERROR] Validation failed:\n";
-                foreach ($this->User->invalidFields() as $x => $err) {
-                    foreach (array_unique($err) as $e) {
-                        echo "[ERROR] Field \"${x}\" -> ${e}\n";
-                    }
-                }
-                break;
-            } else {
-                echo "[INFO] User ".$this->_element['email']." saved successfully\n";
-            }
-        }
+        $this->out('<error>Error:</error> Please specify a method (import, export)', 1, Shell::NORMAL);
     }
 
 }
