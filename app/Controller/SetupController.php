aae14b8180ae5fd4ff068ab00a109a435a092fb3
Andy Newton <amn@ecs.soton.ac.uk>
2013-01-27 18:27:02 +0000

    #180 Make SetupController conform to CodeSniffer standards (various whitespace infractions)

--- app/Controller/SetupController.php
+++ app/Controller/SetupController.php
@@ -1,18 +1,18 @@
 <?php
 /**
-*
-* SetupController for the DevTrack system
-* Controller for setup page
-*
-* Licensed under The MIT License
-* Redistributions of files must retain the above copyright notice.
-*
-* @copyright	 DevTrack Development Team 2012
-* @link			http://github.com/SourceKettle/devtrack
-* @package		DevTrack.Controller
-* @since		 DevTrack v 0.1
-* @license		MIT License (http://www.opensource.org/licenses/mit-license.php)
-*/
+ *
+ * SetupController for the DevTrack system
+ * Controller for setup page
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright	 DevTrack Development Team 2012
+ * @link			http://github.com/SourceKettle/devtrack
+ * @package		DevTrack.Controller
+ * @since		 DevTrack v 0.1
+ * @license		MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 App::uses('AppController', 'Controller');
 
