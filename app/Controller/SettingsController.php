af8e8dc6a852c6198c73845af4f774d039bd02f9
Andy Newton <amn@ecs.soton.ac.uk>
2013-01-27 18:30:51 +0000

    #180 Make SettingsController conform to CodeSniffer standards (various whitespace infractions)

--- app/Controller/SettingsController.php
+++ app/Controller/SettingsController.php
@@ -18,11 +18,11 @@ App::uses('AppController', 'Controller');
 
 class SettingsController extends AppController {
 
-	/**
-	 * admin_index method
-	 *
-	 * @return void
-	 */
+/**
+ * admin_index method
+ *
+ * @return void
+ */
 	public function admin_index() {
 		$this->Setting->recursive = 0;
 		$this->set('register', $this->Setting->field('value', array('name' => 'register_enabled')));
@@ -31,13 +31,19 @@ class SettingsController extends AppController {
 
 		// Project options
 		$this->set('features', array(
-			'time'		=> $this->Setting->field('value', array('name' => 'feature_time_enabled')),
-			'source'	 => $this->Setting->field('value', array('name' => 'feature_source_enabled')),
-			'task'		=> $this->Setting->field('value', array('name' => 'feature_task_enabled')),
-			'attachment' => $this->Setting->field('value', array('name' => 'feature_attachment_enabled'))
+			'time'			=> $this->Setting->field('value', array('name' => 'feature_time_enabled')),
+			'source'		=> $this->Setting->field('value', array('name' => 'feature_source_enabled')),
+			'task'			=> $this->Setting->field('value', array('name' => 'feature_task_enabled')),
+			'attachment'	=> $this->Setting->field('value', array('name' => 'feature_attachment_enabled'))
 		));
 	}
 
+/**
+ * admin_setEmail method
+ *
+ * @throws MethodNotAllowedException
+ * @return void
+ */
 	public function admin_setEmail() {
 		// Check form value provided and request is a post request
 		if (!$this->request->is('post')) {
@@ -51,78 +57,78 @@ class SettingsController extends AppController {
 		$this->redirect(array('admin' => true, 'controller' => 'settings', 'action' => 'index'));
 	}
 
-	/**
-	 * admin_setRegistration function.
-	 *
-	 * @access public
-	 * @param mixed $value (default: null)
-	 * @return void
-	 */
+/**
+ * admin_setRegistration function.
+ *
+ * @access public
+ * @param mixed $value (default: null)
+ * @return void
+ */
 	public function admin_setRegistration($value = null) {
 		$this->admin_setField('register_enabled', $value);
 	}
 
-	/**
-	 * admin_setFeatureTime function.
-	 *
-	 * @access public
-	 * @param mixed $value (default: null)
-	 * @return void
-	 */
+/**
+ * admin_setFeatureTime function.
+ *
+ * @access public
+ * @param mixed $value (default: null)
+ * @return void
+ */
 	public function admin_setFeatureTime($value = null) {
 		$this->admin_setField('feature_time_enabled', $value);
 	}
 
-	/**
-	 * admin_setFeatureTask function.
-	 *
-	 * @access public
-	 * @param mixed $value (default: null)
-	 * @return void
-	 */
+/**
+ * admin_setFeatureTask function.
+ *
+ * @access public
+ * @param mixed $value (default: null)
+ * @return void
+ */
 	public function admin_setFeatureTask($value = null) {
 		$this->admin_setField('feature_task_enabled', $value);
 	}
 
-	/**
-	 * admin_setFeatureSource function.
-	 *
-	 * @access public
-	 * @param mixed $value (default: null)
-	 * @return void
-	 */
+/**
+ * admin_setFeatureSource function.
+ *
+ * @access public
+ * @param mixed $value (default: null)
+ * @return void
+ */
 	public function admin_setFeatureSource($value = null) {
 		$this->admin_setField('feature_source_enabled', $value);
 	}
 
-	/**
-	 * admin_setFeatureAttachment function.
-	 *
-	 * @access public
-	 * @param mixed $value (default: null)
-	 * @return void
-	 */
+/**
+ * admin_setFeatureAttachment function.
+ *
+ * @access public
+ * @param mixed $value (default: null)
+ * @return void
+ */
 	public function admin_setFeatureAttachment($value = null) {
 		$this->admin_setField('feature_attachment_enabled', $value);
 	}
 
-	/**
-	 * admin_setField function.
-	 *
-	 * @access private
-	 * @param mixed $field (default: null)
-	 * @param mixed $value (default: null)
-	 * @return void
-	 */
+/**
+ * admin_setField function.
+ *
+ * @access private
+ * @param mixed $field (default: null)
+ * @param mixed $value (default: null)
+ * @return void
+ */
 	private function admin_setField($field = null, $value = null) {
 		$this->Setting->id = $this->Setting->field('id', array('name' => $field));
 		if (!$this->Setting->exists()) {
-			$this->Flash->error('The specified setting does not exist in the database. Please create "'.$field.'" and try again.');
-		} else if (in_array((int) $value, array(0,1))) {
+			$this->Flash->error('The specified setting does not exist in the database. Please create "' . $field . '" and try again.');
+		} else if (in_array((int)$value, array(0,1))) {
 			$this->Setting->set('value', $value);
 			$this->Setting->save();
 		} else {
-			$this->Flash->error('Cannot set "'.$field.'" to a value other than 1 or 0. Please try again.');
+			$this->Flash->error('Cannot set "' . $field . '" to a value other than 1 or 0. Please try again.');
 		}
 		$this->redirect(array('admin' => true, 'controller' => 'settings', 'action' => 'index'));
 	}
