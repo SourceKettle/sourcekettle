f693bb2f3d39371f38e54226df8eaf236c1ae380
Andy Newton <amn@ecs.soton.ac.uk>
2013-01-27 17:26:16 +0000

    #180 Make VersionController conform to CodeSniffer standards (indentation only)

--- app/Controller/VersionController.php
+++ app/Controller/VersionController.php
@@ -9,30 +9,30 @@
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     DevTrack Development Team 2012
- * @link          http://github.com/SourceKettle/devtrack
- * @package       DevTrack.Controller
- * @since         DevTrack v 0.1
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @copyright	 DevTrack Development Team 2012
+ * @link			http://github.com/SourceKettle/devtrack
+ * @package		DevTrack.Controller
+ * @since		 DevTrack v 0.1
+ * @license		MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 App::uses('AppController', 'Controller');
 App::uses('CakeEmail', 'Network/Email');
 
 class VersionController extends AppController {
 
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('api_index');
-    }
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('api_index');
+	}
 
-    /**
-     * Register a user via an API call
-     */
-    public function api_index() {
-        $this->set('data', array(
-            'version' => '0.0.1',
-            ));
-        $this->layout = 'ajax';
-        $this->render('/Elements/json');
-    }
+	/**
+	 * Register a user via an API call
+	 */
+	public function api_index() {
+		$this->set('data', array(
+			'version' => '0.0.1',
+			));
+		$this->layout = 'ajax';
+		$this->render('/Elements/json');
+	}
 }
