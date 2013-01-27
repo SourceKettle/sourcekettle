5ede2582ebbece612d5c48e76a7632db40a4ef47
Andy Newton <amn@ecs.soton.ac.uk>
2013-01-27 17:24:47 +0000

    #180 Make SetupController conform to CodeSniffer standards (indentation only)

--- app/Controller/SetupController.php
+++ app/Controller/SetupController.php
@@ -7,26 +7,26 @@
 * Licensed under The MIT License
 * Redistributions of files must retain the above copyright notice.
 *
-* @copyright     DevTrack Development Team 2012
-* @link          http://github.com/SourceKettle/devtrack
-* @package       DevTrack.Controller
-* @since         DevTrack v 0.1
-* @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+* @copyright	 DevTrack Development Team 2012
+* @link			http://github.com/SourceKettle/devtrack
+* @package		DevTrack.Controller
+* @since		 DevTrack v 0.1
+* @license		MIT License (http://www.opensource.org/licenses/mit-license.php)
 */
 
 App::uses('AppController', 'Controller');
 
 class SetupController extends AppController {
 
-    public $name = 'Setup';
+	public $name = 'Setup';
 
-    public $uses = array();
+	public $uses = array();
 
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow(); //does not require login to use all actions in this controller
-    }
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow(); //does not require login to use all actions in this controller
+	}
 
-    public function index() {
-    }
+	public function index() {
+	}
 }
