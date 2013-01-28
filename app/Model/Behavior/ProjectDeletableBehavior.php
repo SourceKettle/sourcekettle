08f74fb24847c364ce090bde532ce0ec0e3938cc
Phillip Whittlesea <pw.github@thega.me.uk>
2013-01-28 21:33:56 +0000

    Issue #180 - Update ProjectDeletableBehavior

--- app/Model/Behavior/ProjectDeletableBehavior.php
+++ app/Model/Behavior/ProjectDeletableBehavior.php
@@ -15,62 +15,62 @@
 
 class ProjectDeletableBehavior extends ModelBehavior {
 
-    var $settings = array();
+	public $settings = array();
 
-    var $model = null;
+	public $model = null;
 
-    var $ignore = array('ProjectHistory');
+	public $ignore = array('ProjectHistory');
 
-    public function setup(Model $model, $settings = array()) {
-        $this->settings[$model->name] = $settings;
-        $this->model = &$model;
-    }
+	public function setup(Model $model, $settings = array()) {
+		$this->settings[$model->name] = $settings;
+		$this->model = &$model;
+	}
 
-    /**
-     * preDelete function.
-     *
-     * @access public
-     * @param Model $Model
-     * @return void
-     */
-    public function preDelete(Model $Model) {
-        $objects = $this->preDeleteR($Model, array('id' => $Model->id));
-        $ignore = $this->ignore;
-        $ignore[] = $Model->name;
-        foreach ($ignore as $i) {
-            if (isset($objects[$i])) {
-                unset($objects[$i]);
-            }
-        }
-        return $objects;
-    }
+/**
+ * preDelete function.
+ *
+ * @access public
+ * @param Model $Model
+ * @return void
+ */
+	public function preDelete(Model $Model) {
+		$objects = $this->__preDeleteR($Model, array('id' => $Model->id));
+		$ignore = $this->ignore;
+		$ignore[] = $Model->name;
+		foreach ($ignore as $i) {
+			if (isset($objects[$i])) {
+				unset($objects[$i]);
+			}
+		}
+		return $objects;
+	}
 
-    /**
-     * preDeleteR function.
-     *
-     * @access private
-     * @param mixed $Model
-     * @param array $conditions (default: array())
-     * @return void
-     */
-    private function preDeleteR($Model, $conditions = array()) {
-        $Model->recursive = -1;
+/**
+ * __preDeleteR function.
+ *
+ * @access private
+ * @param mixed $Model
+ * @param array $conditions (default: array())
+ * @return void
+ */
+	private function __preDeleteR($Model, $conditions = array()) {
+		$Model->recursive = -1;
 
-        $objects = array($Model->name => $Model->find('list', array('conditions' => $conditions)));
-        $_list = array_values($Model->find('list', array('conditions' => $conditions, 'fields' => array('id'))));
-        foreach ($Model->hasMany as $key => $value) {
-            if ($value['dependent']) {
-                $_objects = $this->preDeleteR($Model->{$key}, array($value['foreignKey'] => $_list));
-                foreach ($_objects as $_k => $_v) {
-                    if (isset($objects[$_k])) {
-                        $objects[$_k] = array_merge($objects[$_k], $_objects[$_k]);
-                    } else {
-                        $objects[$_k] = $_objects[$_k];
-                    }
-                }
-            }
-        }
-        return $objects;
-    }
+		$objects = array($Model->name => $Model->find('list', array('conditions' => $conditions)));
+		$_list = array_values($Model->find('list', array('conditions' => $conditions, 'fields' => array('id'))));
+		foreach ($Model->hasMany as $key => $value) {
+			if ($value['dependent']) {
+				$_objects = $this->__preDeleteR($Model->{$key}, array($value['foreignKey'] => $_list));
+				foreach ($_objects as $_k => $_v) {
+					if (isset($objects[$_k])) {
+						$objects[$_k] = array_merge($objects[$_k], $_objects[$_k]);
+					} else {
+						$objects[$_k] = $_objects[$_k];
+					}
+				}
+			}
+		}
+		return $objects;
+	}
 
 }
