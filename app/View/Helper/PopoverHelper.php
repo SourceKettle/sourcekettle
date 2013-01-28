f473cf57cb1e5e3ce07679fc5a6f40224a7a3ebe
Andy Newton <amn@ecs.soton.ac.uk>
2013-01-28 09:49:32 +0000

    #180 Make PopoverHelper conform to CodeSniffer standards (various whitespace infractions)

--- app/View/Helper/PopoverHelper.php
+++ app/View/Helper/PopoverHelper.php
@@ -19,24 +19,24 @@ class PopoverHelper extends AppHelper {
 
 	var $helpers = array('Html');
 
-	/*
-	 * popover
-	 * Wrapper for a Html link that will render a popover
-	 *
-	 * @param $a_text string the text to display on page
-	 * @param $p_title string the title for the popover
-	 * @param $p_content string the concent for the popover
-	 */
+/**
+ * popover
+ * Wrapper for a Html link that will render a popover
+ *
+ * @param $a_text string the text to display on page
+ * @param $p_title string the title for the popover
+ * @param $p_content string the concent for the popover
+ */
 	public function popover($a_text, $p_title, $p_content) {
 		$this->hasBeenCalled = true;
 		return $this->Html->link($a_text, '#', array('class' => 'popover-devtrack', 'rel' => 'popover', 'data-content' => $p_content, 'data-original-title' => $p_title));
 	}
 
-	/*
-	 * requirements
-	 * Will return the requirements for popover
-	 *
-	 */
+/**
+ * requirements
+ * Will return the requirements for popover
+ *
+ */
 	public function requirements() {
 		if ($this->hasBeenCalled) {
 			return $this->Html->script(array('bootstrap-tooltip', 'bootstrap-popover')) . $this->Html->scriptBlock("$('.popover-devtrack').popover()");
