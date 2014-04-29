811834f1e9b43504ddc724623a6a3b481fdfe182
Andy Newton <amn@ecs.soton.ac.uk>
2014-04-29 16:40:06 +0100

    change fixtures to import fields from the model classes instead of duplicating

--- app/Test/Fixture/TimeFixture.php
+++ app/Test/Fixture/TimeFixture.php
@@ -2,30 +2,34 @@
 
 class TimeFixture extends CakeTestFixture {
 
-    public $fields = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
-        'project_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-        'user_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10),
-        'task_id' => array('type' => 'integer', 'null' => true, 'default' => NULL, 'length' => 10),
-        'mins' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 10),
-        'description' => array('type' => 'text', 'null' => true, 'default' => NULL),
-        'date' => array('type' => 'date', 'null' => false, 'default' => NULL),
-        'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-        'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
-        'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-    );
+	public $import = array('model' => 'Time');
 
     public $records = array(
         array(
             'id' => 1,
             'project_id' => 1,
             'user_id' => 1,
-            'task_id' => null,
+            'task_id' => 1,
             'mins' => 90,
             'description' => 'A description.',
             'date' => '2012-11-11',
             'created' => '2012-11-11 10:24:06',
-            'modified' => '2012-11-11 10:24:06'
+            'modified' => '2012-11-11 10:24:06',
+			'deleted' => '0',
+			'deleted_date' => null
+        ),
+        array(
+            'id' => 2,
+            'project_id' => 1,
+            'user_id' => 1,
+            'task_id' => 2,
+            'mins' => 900,
+            'description' => 'A description of the second <b>task</b>.',
+            'date' => '2012-11-11',
+            'created' => '2012-11-11 10:24:06',
+            'modified' => '2012-11-11 10:24:06',
+			'deleted' => '0',
+			'deleted_date' => null
         ),
     );
 }
