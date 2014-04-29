1ead3ad5aab262552c9c2c5ee50ef38037602f38
Andy Newton <amn@ecs.soton.ac.uk>
2014-04-29 07:32:30 +0100

    make sure all model tests are present...

--- app/Test/Case/AllModelTestsTest.php
+++ app/Test/Case/AllModelTestsTest.php
@@ -7,11 +7,16 @@ class AllModelTests extends PHPUnit_Framework_TestSuite {
 
 		$path = APP_TEST_CASES . DS;
 
-		$suite->addTestFile($path . 'Model/ProjectTest.php');
-		$suite->addTestFile($path . 'Model/UserTest.php');
 		$suite->addTestFile($path . 'Model/CollaboratorTest.php');
+		$suite->addTestFile($path . 'Model/EmailConfirmationKeyTest.php');
+		$suite->addTestFile($path . 'Model/LostPasswordKeyTest.php');
+		$suite->addTestFile($path . 'Model/ProjectTest.php');
+		//$suite->addTestFile($path . 'Model/RepoTypeTest.php');
 		$suite->addTestFile($path . 'Model/SettingTest.php');
+		//$suite->addTestFile($path . 'Model/SourceTest.php');
+		$suite->addTestFile($path . 'Model/SshKeyTest.php');
 		$suite->addTestFile($path . 'Model/TimeTest.php');
+		$suite->addTestFile($path . 'Model/UserTest.php');
 		return $suite;
 	}
 }
