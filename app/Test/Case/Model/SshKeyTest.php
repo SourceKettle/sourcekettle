406d6f021a92aaa7f2df5a5ff4dd3e7fd0094baa
Andy Newton <amn@ecs.soton.ac.uk>
2014-05-05 20:47:36 +0100

    Fix #349, auto-add SSH key type and comment if possible

--- app/Test/Case/Model/SshKeyTest.php
+++ app/Test/Case/Model/SshKeyTest.php
@@ -16,6 +16,11 @@ App::uses('SshKey', 'Model');
 
 class SshKeyTestCase extends CakeTestCase {
 
+	// Example RSA and DSA public keys, generated specifically for this test.
+		private $dsa =	'ssh-dss AAAAB3NzaC1kc3MAAACBAMy3kgD8D2xr2HwRpz93bi/kVeusYlkz9NFXRQ6E0Ar8xiWBh1lXxWjxCfM+DSRIgdvVJA2AQi3coWfTMXTRiWJAlEJtf3eBWMvJhCeJZ40WChJfbnoRCEORsfwXoo98XigXnWrAMjfqU0xWCvCI+sdRnffUYdHXOgKwmaylPvgRAAAAFQD7AICGcDlVJCJeHu4PfAGmD4dppwAAAIEAr7gHxCATf+7N/3powMWhaJSU5fTWPtSDyDV/fTMOmwd1dvlzW2HToS42gqeg1eyw1C1FVCn/5ptB6u9Qaw3YG7Bvo79wFM1ACpDF0U4f+ZydKC8INhRt4FqUkEjJXgceKa3BAGJncLzLd0fX3TxRf2ZksQx5dV6KGFpyIa5Ii80AAACAS7fEUZgWWIJBklW18nW346jM6KAX5gdAd6j+BJ5QJ1nSxD7ZaYxKEHr7HSdNWOEyJMls4SEzX4opi+ZBVtI1shU7wclxYss0vLuBQV2DitChO9ouQsfRifRMMUB1n8KlSLNAtICjS6pa/TmlhZt9NY+oTYf76BTQFMpEsvjRP20= somebody@localhost';
+
+		private $rsa =	'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyEoCvRtgCa0+yPSuopvo2mFXnF6myTvljXujIBVVQkvk1uPWL8JU8tPBGp7xjS2nC6teHd6Ju2LItZmsLuH+U80BVuhdtGPPc4Iim8j2eEJATJRi7MDxdqlml7c1Ofa6SSwWPUsZFFlBn8KyOWn/V5EdCbBvdg8XLPWb5BhBKHBLm826tBewq7V1teCKNxS5viM7YZRibTzmuuBH7EN8Z0Qb1JMWTWnXQZM6cB9jNJfqyCOOKFWWiq+aBqtfAW58rAfISeIjxwETFYfi8DKIyGHn53MsdNwGBPH99SZnFGq43DVciYioa9t0ZX8MqDhxIhDuQOvAitORiEzmsPZ2F somebody@localhost';
+
     /**
      * fixtures - Populate the database with data of the following models
      */
@@ -70,4 +75,119 @@ class SshKeyTestCase extends CakeTestCase {
         $fixturesB = $this->SshKey->find('all');
         $this->assertEquals($fixtures, $fixturesB, "Arrays were not equal");
     }
+
+	public function atestMissingUserID() {
+		$saved = $this->SshKey->save(array(
+			'comment' => 'some comment',
+			'key' => $this->rsa
+		));
+
+		$this->assertFalse($saved, 'Save RSA key succeeded despite missing user ID');
+		
+	}
+
+	public function atestMissingKey() {
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'comment' => 'some comment',
+		));
+		$this->assertFalse($saved, 'Save RSA key succeeded despite missing SSH key');
+		
+	}
+
+	public function testAddKeyOnly() {
+
+		// Remove comments so we just have the key part
+		$rsa = preg_replace('/ somebody@localhost$/', '', $this->rsa);
+		$dsa = preg_replace('/ somebody@localhost$/', '', $this->dsa);
+
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'comment' => 'some comment',
+			'key' => $rsa
+		));
+
+		$this->assertNotEqual($saved, null, 'Failed to save RSA SSH key');
+		$this->assertNotEqual($saved['SshKey']['id'], null, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['user_id'], 2, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['key'], $rsa, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['comment'], 'some comment', 'Failed to save RSA SSH key');
+
+		$this->SshKey->create();
+
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'comment' => 'some other DSA comment',
+			'key' => $dsa
+		));
+
+		$this->assertNotEqual($saved, null, 'Failed to save DSA SSH key');
+		$this->assertNotEqual($saved['SshKey']['id'], null, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['user_id'], 2, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['key'], $dsa, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['comment'], 'some other DSA comment', 'Failed to save DSA SSH key');
+
+	}
+
+
+	public function testAddKeyWithComment() {
+		$rsa = $this->rsa;
+		$dsa = $this->dsa;
+
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'key' => $rsa
+		));
+
+		$this->assertNotEqual($saved, null, 'Failed to save RSA SSH key');
+		$this->assertNotEqual($saved['SshKey']['id'], null, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['user_id'], 2, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['key'], preg_replace('/ somebody@localhost$/', '', $this->rsa), 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['comment'], 'somebody@localhost', 'Failed to save RSA SSH key');
+
+		$this->SshKey->create();
+
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'key' => $dsa
+		));
+
+		$this->assertNotEqual($saved, null, 'Failed to save DSA SSH key');
+		$this->assertNotEqual($saved['SshKey']['id'], null, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['user_id'], 2, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['key'], preg_replace('/ somebody@localhost$/', '', $this->dsa), 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['comment'], 'somebody@localhost', 'Failed to save DSA SSH key');
+	}
+
+	public function testAddKeyWithoutFormatType() {
+
+		// Remove the leading format types
+		$rsa = preg_replace('/^ssh-rsa /', '', $this->rsa);
+		$dsa = preg_replace('/^ssh-dss /', '', $this->dsa);
+
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'key' => $rsa
+		));
+
+		$this->assertNotEqual($saved, null, 'Failed to save RSA SSH key');
+		$this->assertNotEqual($saved['SshKey']['id'], null, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['user_id'], 2, 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['key'], preg_replace('/ somebody@localhost$/', '', $this->rsa), 'Failed to save RSA SSH key');
+		$this->assertEqual($saved['SshKey']['comment'], 'somebody@localhost', 'Failed to save RSA SSH key');
+
+		$this->SshKey->create();
+
+		$saved = $this->SshKey->save(array(
+			'user_id' => 2,
+			'key' => $dsa
+		));
+
+		$this->assertNotEqual($saved, null, 'Failed to save DSA SSH key');
+		$this->assertNotEqual($saved['SshKey']['id'], null, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['user_id'], 2, 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['key'], preg_replace('/ somebody@localhost$/', '', $this->dsa), 'Failed to save DSA SSH key');
+		$this->assertEqual($saved['SshKey']['comment'], 'somebody@localhost', 'Failed to save DSA SSH key');
+		
+	}
 }
