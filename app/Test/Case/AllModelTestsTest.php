d022d957a7fdbc5ea769a299c47ddd7dc0b9ebb8
Andy Newton <amn@ecs.soton.ac.uk>
2014-05-08 19:10:34 +0100

    Add in all model test suites

--- app/Test/Case/AllModelTestsTest.php
+++ app/Test/Case/AllModelTestsTest.php
@@ -7,19 +7,23 @@ class AllModelTests extends PHPUnit_Framework_TestSuite {
 
 		$path = APP_TEST_CASES . DS;
 
+		$suite->addTestFile($path . 'Model/AttachmentTest.php');
 		$suite->addTestFile($path . 'Model/CollaboratorTest.php');
 		$suite->addTestFile($path . 'Model/EmailConfirmationKeyTest.php');
 		$suite->addTestFile($path . 'Model/LostPasswordKeyTest.php');
+		$suite->addTestFile($path . 'Model/MilestoneTest.php');
+		//$suite->addTestFile($path . 'Model/ProjectHistoryTest.php'); // No fixtures yet
 		$suite->addTestFile($path . 'Model/ProjectTest.php');
-		//$suite->addTestFile($path . 'Model/RepoTypeTest.php');
+		//$suite->addTestFile($path . 'Model/RepoTypeTest.php'); // No code in model
 		$suite->addTestFile($path . 'Model/SettingTest.php');
-		//$suite->addTestFile($path . 'Model/SourceTest.php');
+		$suite->addTestFile($path . 'Model/SourceTest.php');
 		$suite->addTestFile($path . 'Model/SshKeyTest.php');
-		$suite->addTestFile($path . 'Model/TimeTest.php');
-		$suite->addTestFile($path . 'Model/TaskTest.php');
+		$suite->addTestFile($path . 'Model/TaskCommentTest.php');
 		$suite->addTestFile($path . 'Model/TaskPriorityTest.php');
 		$suite->addTestFile($path . 'Model/TaskStatusTest.php');
+		$suite->addTestFile($path . 'Model/TaskTest.php');
 		$suite->addTestFile($path . 'Model/TaskTypeTest.php');
+		$suite->addTestFile($path . 'Model/TimeTest.php');
 		$suite->addTestFile($path . 'Model/UserTest.php');
 		return $suite;
 	}
