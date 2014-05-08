333d6478534bbc05a503047ccfd87164bb60b887
Andy Newton <amn@ecs.soton.ac.uk>
2014-05-08 11:22:19 +0100

    Test for TaskPriority

--- app/Test/Case/AllModelTestsTest.php
+++ app/Test/Case/AllModelTestsTest.php
@@ -16,6 +16,8 @@ class AllModelTests extends PHPUnit_Framework_TestSuite {
 		//$suite->addTestFile($path . 'Model/SourceTest.php');
 		$suite->addTestFile($path . 'Model/SshKeyTest.php');
 		$suite->addTestFile($path . 'Model/TimeTest.php');
+		$suite->addTestFile($path . 'Model/TaskTest.php');
+		$suite->addTestFile($path . 'Model/TaskPriorityTest.php');
 		$suite->addTestFile($path . 'Model/UserTest.php');
 		return $suite;
 	}
