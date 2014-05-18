22036b64527479d209be26363877451e4fdfbb34
Andy Newton <amn@ecs.soton.ac.uk>
2014-05-18 22:56:21 +0100

    full coverage of shiftTasks

--- app/Test/Case/Model/MilestoneTest.php
+++ app/Test/Case/Model/MilestoneTest.php
@@ -583,7 +583,241 @@ class MilestoneTest extends CakeTestCase {
  */
  	// TODO requires currently logged-in user to be set
 	public function testShiftTasks() {
-		//$this->Milestone->shiftTasks(2, 4);
+
+		$open = array(
+			array(
+				'Task' => array(
+					'id' => '5',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '1',
+					'task_priority_id' => '3',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '20h 45m',
+					'story_points' => '0',
+					'subject' => 'Open Urgent Task 5 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '5'
+				)
+			),
+			array(
+				'Task' => array(
+					'id' => '2',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '1',
+					'task_priority_id' => '1',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '4h 0m',
+					'story_points' => '0',
+					'subject' => 'Open Minor Task 2 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '2'
+				)
+			)
+		);
+		$in_progress = array(
+			array(
+				'Task' => array(
+					'id' => '6',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '2',
+					'task_priority_id' => '4',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '2h 25m',
+					'story_points' => '0',
+					'subject' => 'In Progress Blocker Task 7 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '6'
+				)
+			),
+			array(
+				'Task' => array(
+					'id' => '4',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '2',
+					'task_priority_id' => '3',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '2h 25m',
+					'story_points' => '0',
+					'subject' => 'In progress Urgent Task 4 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '4'
+				)
+			)
+		);
+		$resolved = array(
+			array(
+				'Task' => array(
+					'id' => '7',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '3',
+					'task_priority_id' => '2',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '2h 25m',
+					'story_points' => '0',
+					'subject' => 'Resolved Major Task 7 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '7'
+				)
+			)
+		);
+		$completed = array(
+			array(
+				'Task' => array(
+					'id' => '8',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '4',
+					'task_priority_id' => '4',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '2h 25m',
+					'story_points' => '0',
+					'subject' => 'Closed Blocker Task 8 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '8'
+				)
+			)
+		);
+		$dropped = array(
+			array(
+				'Task' => array(
+					'id' => '9',
+					'project_id' => '2',
+					'owner_id' => '3',
+					'task_type_id' => '1',
+					'task_status_id' => '5',
+					'task_priority_id' => '1',
+					'assignee_id' => '0',
+					'milestone_id' => '1',
+					'time_estimate' => '2h 25m',
+					'story_points' => '0',
+					'subject' => 'Dropped Minor Task 9 for milestone 1',
+					'description' => 'lorem ipsum dolor sit amet',
+					'created' => '0000-00-00 00:00:00',
+					'modified' => '0000-00-00 00:00:00',
+					'deleted' => '0',
+					'deleted_date' => null,
+					'public_id' => '9'
+				)
+			)
+		);
+
+		// Should do nothing if we pass in null...
+		$this->assertFalse($this->Milestone->shiftTasks(null));
+
+		// Load in both milestones' data before moving
+		//$this->Milestone->recursive = -1;
+		$milestone1_pre = $this->Milestone->findById(1);
+		$milestone3_pre = $this->Milestone->findById(3);
+		$this->assertEqual($milestone1_pre['Tasks'], array(
+			'open' => $open,
+			'in_progress' => $in_progress,
+			'resolved' => $resolved,
+			'completed' => $completed,
+			'dropped' => $dropped
+		));
+		$this->assertEqual($milestone3_pre['Tasks'], array(
+			'open' => array(),
+			'in_progress' => array(),
+			'resolved' => array(),
+			'completed' => array(),
+			'dropped' => array(),
+		));
+
+		// Shift only the non-resolved/closed tasks and check all is well
+		$this->Milestone->shiftTasks(1, 3, false, array('callbacks' => false));
+		foreach ($open as $id => $task) {
+			$open[$id]['Task']['milestone_id'] = 3;
+		}
+		foreach ($in_progress as $id => $task) {
+			$in_progress[$id]['Task']['milestone_id'] = 3;
+		}
+		foreach ($dropped as $id => $task) {
+			$dropped[$id]['Task']['milestone_id'] = 3;
+		}
+
+		$milestone1_post = $this->Milestone->findById(1);
+		$milestone3_post = $this->Milestone->findById(3);
+		$this->assertEqual($milestone1_post['Tasks'], array(
+			'open' => array(),
+			'in_progress' => array(),
+			'resolved' => $resolved,
+			'completed' => $completed,
+			'dropped' => array()
+		));
+		$this->assertEqual($milestone3_post['Tasks'], array(
+			'open' => $open,
+			'in_progress' => $in_progress,
+			'resolved' => array(),
+			'completed' => array(),
+			'dropped' => $dropped
+		));
+
+		// Now try shifting all tasks and make sure that also works
+		$this->Milestone->shiftTasks(1, 3, true, array('callbacks' => false));
+		foreach ($resolved as $id => $task) {
+			$resolved[$id]['Task']['milestone_id'] = 3;
+		}
+		foreach ($completed as $id => $task) {
+			$completed[$id]['Task']['milestone_id'] = 3;
+		}
+
+		$milestone1_post = $this->Milestone->findById(1);
+		$milestone3_post = $this->Milestone->findById(3);
+		$this->assertEqual($milestone1_post['Tasks'], array(
+			'open' => array(),
+			'in_progress' => array(),
+			'resolved' => array(),
+			'completed' => array(),
+			'dropped' => array()
+		));
+		$this->assertEqual($milestone3_post['Tasks'], array(
+			'open' => $open,
+			'in_progress' => $in_progress,
+			'resolved' => $resolved,
+			'completed' => $completed,
+			'dropped' => $dropped
+		));
 	}
 
 /**
@@ -592,6 +826,18 @@ class MilestoneTest extends CakeTestCase {
  * @return void
  */
 	public function testFetchHistory() {
+		
 	}
+
+	// TODO we need a way to fake the currently logged in user before we can test the beforeDelete callback
+	/*public function testDelete() {
+		$milestone_pre = $this->Milestone->findById(1);
+
+		$this->Milestone->deleteAll(array('Milestone.id' => 1), false, false);
+		$milestone_post = $this->Milestone->findById(1);
+
+		debug($milestone_pre);
+		debug($milestone_post);
+	}*/
 
 }
