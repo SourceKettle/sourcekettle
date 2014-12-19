823d6cdd547641e81a8aa564c6d47181acd18a4f
Andy Newton <amn@ecs.soton.ac.uk>
2014-12-19 09:16:28 +0000

    add team and project group console commands for #314

---
+++ app/Console/Command/ProjectGroupShell.php
@@ -0,0 +1,340 @@
+<?php
+
+/**
+ * ProjectGroupShell - admin commands to add/remove user accounts on the command line,
+ * and to set/unset admin priveleges etc.
+ *
+ * @author amn@ecs.soton.ac.uk
+ */
+class ProjectGroupShell extends AppShell {
+
+	public $uses = array('ProjectGroup', 'Project', 'ProjectGroupsProject', 'Security', 'Team', 'GroupCollaboratingTeam');
+	// TODO this should be centralised somewhere?
+	private $accessLevels = array(
+		'guest' => 0,
+		'user'  => 1,
+		'admin' => 2,
+	);
+
+	public function getOptionParser() {
+		$parser = parent::getOptionParser();
+
+		$parser->addSubCommand('add', array(
+			'help' => __('Create a new project group'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to add a new project group to the system."),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The project group name"),
+						'required' => true,
+					),
+				),
+				'options' => array(
+					'description' => array(
+						'short' => 'd',
+						'help' => __("The longer project description"),
+					),
+					'projects' => array(
+						'short' => 'p',
+						'help' => __("Comma-separated list of names of projects to add to the group"),
+					),
+				)
+			)
+		));
+
+		$parser->addSubCommand('addProject', array(
+			'help' => __('Adds a project to the project group'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to add a project to the project group"),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The project group name"),
+						'required' => true,
+					),
+					'project' => array(
+						'help' => __("The project name"),
+						'required' => true,
+					),
+				),
+			),
+		));
+
+		$parser->addSubCommand('removeProject', array(
+			'help' => __('Remove a project from the project group'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to remove a project from a project group"),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The project group name"),
+						'required' => true,
+					),
+					'project' => array(
+						'help' => __("The project name"),
+						'required' => true,
+					),
+				),
+			),
+		));
+		$parser->addSubCommand('addTeam', array(
+			'help' => __('Grants a team access to all projects in the group as collaborators'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to add a team as collaborators. If the team is already collaborating, you can set their access level."),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The project group name"),
+						'required' => true,
+					),
+					'team' => array(
+						'help' => __("The team name"),
+						'required' => true,
+					),
+				),
+				'options' => array(
+					'type' => array(
+						'short' => 't',
+						'help' => __("The team's access level"),
+						'choices' => array('guest', 'user', 'admin'),
+						'default' => 'user',
+					),
+				),
+			),
+		));
+
+		$parser->addSubCommand('removeTeam', array(
+			'help' => __('Revokes a team\'s access to the project group'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to revoke a team's access to a project group."),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The project group name"),
+						'required' => true,
+					),
+					'team' => array(
+						'help' => __("The team name"),
+						'required' => true,
+					),
+				),
+			),
+		));
+
+		return $parser;
+	}
+
+/**
+ * Adds a new project group
+ */
+	public function add() {
+
+		$name = $this->args[0];
+
+		// Find existing group
+		$found = $this->ProjectGroup->findByName($name);
+
+		// If it exists, error out
+		if (!empty($found)) {
+			$this->error(__("Found an existing project group with that name!"));
+		}
+
+		$this->out(__("Creating a new project group..."));
+
+		$this->params['projects'] = array_map('trim', split(',', @$this->params['projects']));
+
+		$projects = array();
+		foreach ($this->params['projects'] as $projectName) {
+			if (empty($projectName)) {
+				continue;
+			}
+
+			$project = $this->Project->findByName($projectName);
+			if (!$project) {
+				$this->error("$projectName is not a recognised project!");
+			}
+			$projects[$project['Project']['id']] = array('project_id' => $project['Project']['id']);
+		}
+
+		$this->ProjectGroup->create();
+
+		$ok = $this->ProjectGroup->saveAll(array(
+			'ProjectGroup' => array(
+				'name'      => $name,
+				'description' => @$this->params['description'],
+				'deleted'   => 0,
+			),
+			'Project' => $projects
+		));
+
+		if (!$ok) {
+			$this->error(__("Failed to create project group '$name'!"));
+		}
+
+		$project = $this->ProjectGroup->findByName($name);
+		$this->out(__("ProjectGroup '$name' created with ID ".$project['ProjectGroup']['id']));
+		
+	}
+
+	public function addProject() {
+
+		$projectGroupName = $this->args[0];
+		$projectName = $this->args[1];
+
+		$this->Project->recursive = -1;
+		$projectGroupDetails = $this->ProjectGroup->findByName($projectGroupName);
+		$projectDetails = $this->Project->findByName($projectName);
+
+		if (!$projectGroupDetails) {
+			$this->error(__("Could not find a project group named '$projectGroupName'!"));
+		}
+
+		if (!$projectDetails) {
+			$this->error(__("Could not find a project named '$projectName'!"));
+		}
+
+		$projectGroupId = $projectGroupDetails['ProjectGroup']['id'];
+
+		// Check existing collaborator list
+		foreach ($projectGroupDetails['Project'] as $member) {
+
+			// Don't need to do anything :-)
+			if ($member['id'] == $projectDetails['Project']['id']) {
+				$this->out(__("$projectName is already a member of the group $projectGroupName."));
+				return;
+			}
+		}
+
+		// Need to add a new collaborator link
+		$ok = $this->ProjectGroupsProject->save(array('ProjectGroupsProject' => array(
+			'project_group_id' => $projectGroupDetails['ProjectGroup']['id'],
+			'project_id'    => $projectDetails['Project']['id'],
+		)), array('callbacks' => false));
+
+		if (!$ok) {
+			$this->error(__("Failed to add project!"));
+		}
+
+		$this->out(__("$projectName is now a member of $projectGroupName"));
+	}
+
+	public function removeProject() {
+
+		$projectGroupName = $this->args[0];
+		$projectName   = $this->args[1];
+
+		$this->Project->recursive = -1;
+		$projectGroupDetails = $this->ProjectGroup->findByName($projectGroupName);
+		$projectDetails = $this->Project->findByName($projectName);
+
+		if (!$projectGroupDetails) {
+			$this->error(__("Could not find a project group named '$projectGroupName'!"));
+			return;
+		}
+
+		if (!$projectDetails) {
+			$this->error(__("Could not find a project named '$projectName'!"));
+			return;
+		}
+
+		$memberDetails = $this->ProjectGroupsProject->findByProjectGroupIdAndProjectId($projectGroupDetails['ProjectGroup']['id'], $projectDetails['Project']['id']);
+		if (!$memberDetails) {
+			$this->out(__("$projectName is not a member of $projectGroupName"));
+			return;
+		}
+		
+		$ok = $this->ProjectGroupsProject->deleteAll(
+			array('ProjectGroupsProject.id' => $memberDetails['ProjectGroupsProject']['id']),
+		false, false);
+
+		if (!$ok) {
+			$this->error(__("Failed to remove $projectName from $projectGroupName"));
+			return;
+		}
+
+		$this->out(__("$projectName is no longer a member of $projectGroupName"));
+	}
+
+	public function addTeam() {
+
+		$projectName = $this->args[0];
+		$teamName = $this->args[1];
+
+		$this->Team->recursive = -1;
+		$projectDetails = $this->ProjectGroup->findByName($projectName);
+		$teamDetails = $this->Team->findByName($teamName);
+
+		// Convert 'user', 'guest' or 'admin' to access level integer
+		$accessLevel = $this->accessLevels[$this->params['type']];
+
+		if (!$projectDetails) {
+			$this->error(__("Could not find a project named '$projectName'!"));
+		}
+
+		if (!$teamDetails) {
+			$this->error(__("Could not find a team named '$teamName'!"));
+		}
+
+		$teamId = $teamDetails['Team']['id'];
+
+		// Check existing collaborator list
+		foreach ($projectDetails['GroupCollaboratingTeam'] as $collab) {
+
+			// Don't need to do anything :-)
+			if ($collab['team_id'] == $teamId && $collab['access_level'] == $accessLevel) {
+				$this->out(__("$teamName already has ".$this->params['type']." access to the project."));
+				return;
+
+			// Found them, wrong status, stop checking
+			} elseif ($collab['team_id'] == $teamId) {
+				$this->GroupCollaboratingTeam->id = $collab['id'];
+				break;
+			}
+		}
+
+		// Need to add a new collaborator link
+		$ok = $this->GroupCollaboratingTeam->save(array('GroupCollaboratingTeam' => array(
+			'project_id' => $projectDetails['ProjectGroup']['id'],
+			'team_id'    => $teamDetails['Team']['id'],
+			'access_level' => $accessLevel,
+		)), array('callbacks' => false));
+
+		if (!$ok) {
+			$this->error(__("Failed to add team!"));
+		}
+
+		$this->out(__("$teamName is now a ".$this->params['type']." on $projectName"));
+	}
+
+	public function removeTeam() {
+
+		$projectName = $this->args[0];
+		$teamName   = $this->args[1];
+
+		$collabDetails = $this->GroupCollaboratingTeam->find('first', array('conditions' => array(
+			'Team.name' => $teamName,
+			'ProjectGroup.name' => $projectName,
+		)));
+
+		if (!$collabDetails) {
+			$this->error(__("$teamName is not collaborating on $projectName!"));
+		}
+		$ok = $this->GroupCollaboratingTeam->deleteAll(
+			array('GroupCollaboratingTeam.id' => $collabDetails['GroupCollaboratingTeam']['id']),
+		false, false);
+
+		if (!$ok) {
+			$this->error(__("Failed to remove collaborator!"));
+		}
+
+		$this->out(__("$teamName is no longer collaborating on $projectName"));
+	}
+
+}
+
---
+++ app/Console/Command/TeamShell.php
@@ -0,0 +1,212 @@
+<?php
+
+/**
+ * TeamShell - admin commands to add/remove teams on the command line,
+ * and to add/remove team members.
+ *
+ * @author amn@ecs.soton.ac.uk
+ */
+class TeamShell extends AppShell {
+
+	public $uses = array('Team', 'User', 'TeamsUser', 'Security', 'Team');
+
+	public function getOptionParser() {
+		$parser = parent::getOptionParser();
+
+		$parser->addSubCommand('add', array(
+			'help' => __('Create a new team'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to add a new team to the system."),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The team name"),
+						'required' => true,
+					),
+				),
+				'options' => array(
+					'description' => array(
+						'short' => 'd',
+						'help' => __("The longer teamdescription"),
+					),
+					'users' => array(
+						'short' => 'p',
+						'help' => __("Comma-separated list of names of users to add to the team"),
+					),
+				)
+			)
+		));
+
+		$parser->addSubCommand('addUser', array(
+			'help' => __('Adds a user to a team'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to add a user to a team"),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The team name"),
+						'required' => true,
+					),
+					'user' => array(
+						'help' => __("The user name"),
+						'required' => true,
+					),
+				),
+			),
+		));
+
+		$parser->addSubCommand('removeUser', array(
+			'help' => __('Remove a user from the team'),
+			'parser' => array(
+				'description' => array(
+					__("Use this command to remove a user from a team"),
+				),
+				'arguments' => array(
+					'name' => array(
+						'help' => __("The team name"),
+						'required' => true,
+					),
+					'user' => array(
+						'help' => __("The user name"),
+						'required' => true,
+					),
+				),
+			),
+		));
+		return $parser;
+	}
+
+/**
+ * Adds a new team
+ */
+	public function add() {
+
+		$name = $this->args[0];
+
+		// Find existing group
+		$found = $this->Team->findByName($name);
+
+		// If it exists, error out
+		if (!empty($found)) {
+			$this->error(__("Found an existing team with that name!"));
+		}
+
+		$this->out(__("Creating a new team..."));
+
+		$this->params['users'] = array_map('trim', split(',', @$this->params['users']));
+
+		$users = array();
+		foreach ($this->params['users'] as $userEmail) {
+			if (empty($userEmail)) {
+				continue;
+			}
+
+			$user = $this->User->findByEmail($userEmail);
+			if (!$user) {
+				$this->error("$userEmail is not a recognised user!");
+			}
+			$users[$user['User']['id']] = array('user_id' => $user['User']['id']);
+		}
+
+		$this->Team->create();
+
+		$ok = $this->Team->saveAll(array(
+			'Team' => array(
+				'name'      => $name,
+				'description' => @$this->params['description'],
+				'deleted'   => 0,
+			),
+			'User' => $users
+		));
+
+		if (!$ok) {
+			$this->error(__("Failed to create team '$name'!"));
+		}
+
+		$user = $this->Team->findByName($name);
+		$this->out(__("Team '$name' created with ID ".$user['Team']['id']));
+		
+	}
+
+	public function addUser() {
+
+		$teamName = $this->args[0];
+		$userEmail = $this->args[1];
+
+		$this->User->recursive = -1;
+		$teamDetails = $this->Team->findByName($teamName);
+		$userDetails = $this->User->findByEmail($userEmail);
+
+		if (!$teamDetails) {
+			$this->error(__("Could not find a team named '$teamName'!"));
+		}
+
+		if (!$userDetails) {
+			$this->error(__("Could not find a user named '$userEmail'!"));
+		}
+
+		$teamId = $teamDetails['Team']['id'];
+
+		// Check existing collaborator list
+		foreach ($teamDetails['User'] as $member) {
+
+			// Don't need to do anything :-)
+			if ($member['id'] == $userDetails['User']['id']) {
+				$this->out(__("$userEmail is already a member of the group $teamName."));
+				return;
+			}
+		}
+
+		$ok = $this->TeamsUser->save(array('TeamsUser' => array(
+			'team_id' => $teamDetails['Team']['id'],
+			'user_id' => $userDetails['User']['id'],
+		)));
+
+		if (!$ok) {
+			$this->error(__("Failed to add $userEmail to $teamName!"));
+		}
+
+		$this->out(__("$userEmail is now a member of $teamName"));
+	}
+
+	public function removeUser() {
+
+		$teamName = $this->args[0];
+		$userEmail   = $this->args[1];
+
+		$this->User->recursive = -1;
+		$teamDetails = $this->Team->findByName($teamName);
+		$userDetails = $this->User->findByEmail($userEmail);
+
+		if (!$teamDetails) {
+			$this->error(__("Could not find a team named '$teamName'!"));
+			return;
+		}
+
+		if (!$userDetails) {
+			$this->error(__("Could not find a user named '$userEmail'!"));
+			return;
+		}
+
+		$memberDetails = $this->TeamsUser->findByTeamIdAndUserId($teamDetails['Team']['id'], $userDetails['User']['id']);
+		if (!$memberDetails) {
+			$this->out(__("$userEmail is not a member of $teamName"));
+			return;
+		}
+		
+		$ok = $this->TeamsUser->deleteAll(
+			array('TeamsUser.id' => $memberDetails['TeamsUser']['id']),
+		false, false);
+
+		if (!$ok) {
+			$this->error(__("Failed to remove $userEmail from $teamName"));
+			return;
+		}
+
+		$this->out(__("$userEmail is no longer a member of $teamName"));
+	}
+
+}
+
