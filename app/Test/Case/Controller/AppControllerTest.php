53e30857bca3e80ea9553bfd11d7ae9a3f18a136
Andy Newton <amn@ecs.soton.ac.uk>
2014-12-26 00:03:32 +0000

    undo renaming

--- app/Test/Case/Controller/AppControllerTest.php
+++ app/Test/Case/Controller/AppControllerTest.php
@@ -7,7 +7,7 @@ App::uses('User', 'Model');
  * AppController Test Case
  * Holds common Fixture IDs and mocks for controllers
  */
-class AppControllerTestBase extends ControllerTestCase {
+class AppControllerTest extends ControllerTestCase {
 
     public $authUserId;
 
