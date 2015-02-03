1ac0bd6536f3cbb6eb89e0782cc1a008c7356bdd
Andy Newton <amn@ecs.soton.ac.uk>
2015-02-03 12:46:51 +0000

    Fix gravatars again

--- app/Controller/TasksController.php
+++ app/Controller/TasksController.php
@@ -821,7 +821,12 @@ class TasksController extends AppProjectController {
 		if ($this->request->is('ajax')) {
 			$this->layout = 'ajax';
 			$this->request->data['error'] = 'no_error';
-			$this->request->data['Assignee']['gravatar'] = Gravatar::url($this->request->data['Assignee']['email'], array('d' => 'mm', 'url_only' => true));
+			if (empty($this->request->data['Assignee']['email'])) {
+				$this->request->data['Assignee']['gravatar'] = Gravatar::url($this->request->data['Assignee']['email'], array('d' => 'mm', 'url_only' => true));
+			} else {
+				$this->request->data['Assignee']['gravatar'] = Gravatar::url($this->request->data['Assignee']['email'], array('url_only' => true));
+			}
+
 			$this->set('data', $this->request->data);
 			$this->render('/Elements/json');
 			return;
